7796b21ef8d37ee58cf1c14b7f737f2067e53c50
Junghyeon0329 <sptlsrja@gmail.com>
2025-01-01 20:20:38 +0900

    로그인 모듈화

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,8 +1,45 @@
 import './MainPage.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import API_URLS from '../../api/apiURLS';
 import { useUser } from '../../contexts/UserContext';
 
 function MainPage() {
-    const { user, logoutUser } = useUser();
+    const [state, setState] = useState({
+        email: '', // 로그인 시 입력한 이메일
+        password: '', // 로그인 시 입력한 비밀번호
+        error: '', // 로그인 시 발생한 오류 메시지
+        showSignUp: false, // 회원가입 모달 표시 여부
+        signUpEmail: '', // 회원가입 시 입력한 이메일
+        signUpPassword: '', // 회원가입 시 입력한 비밀번호
+        showResetPassword: false, // 비밀번호 초기화 모달 표시 여부
+        resetEmail: '', // 비밀번호 초기화 시 입력한 이메일
+    });
+    const updateState = (key, value) => {
+        setState((prev) => ({ ...prev, [key]: value }));
+    };
+
+    const { loginUser } = useUser();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const { email, password } = state;
+        if (email === '' || password === '') {
+          updateState('error', '이메일과 비밀번호를 모두 입력해주세요.');
+          return;
+        }
+    
+        try {
+          const response = await axios.post(API_URLS.LOGIN, { email, password });
+          const { access, refresh, user } = response.data;
+    
+          if (access && refresh) {
+            loginUser({ ...user, accessToken: access, refreshToken: refresh });
+          }
+        } catch (error) {
+          updateState('error', '이메일 또는 비밀번호가 잘못되었습니다.');
+        }
+      };
 
     return (
         <div className="Page-Container">
@@ -10,10 +47,62 @@ function MainPage() {
                 <h1>My Application</h1>
             </header>
             <div className="grid-container">
-                <div className="grid-item item1">Item 1</div>
-                <div className="grid-item item2">Item 2</div>
-                <div className="grid-item item3">Item 3</div>
-                <button className="grid-item button">Add New Item</button>
+                <div className="grid-item item1">
+                    <form onSubmit={handleSubmit}>
+                        <h1>로그인</h1>
+                            <div>
+                                <label htmlFor="email">이메일</label>
+                                <input
+                                    type="text"
+                                    id="email"
+                                    value={state.email}
+                                    onChange={(e) => updateState('email', e.target.value)}
+                                    placeholder="이메일을 입력하세요"
+                                    className="input-field"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password">비밀번호</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    value={state.password}
+                                    onChange={(e) => updateState('password', e.target.value)}
+                                    placeholder="비밀번호를 입력하세요"
+                                    className="input-field"
+                                />
+                                </div>
+                            <p
+                                style={{
+                                    color: 'red',
+                                    minHeight: '20px',
+                                    visibility: state.error ? 'visible' : 'hidden',
+                                    marginTop: '10px',
+                                }}
+                            >{state.error}</p>
+
+                        <button type="submit" className="submit-button"> 제출하기 </button>
+                    </form>
+                    <div className="link-container">
+                        <button
+                            onClick={() => {
+                                updateState('signUpEmail', '');
+                                updateState('signUpPassword', '');
+                                // setSignUpError('');
+                                updateState('showSignUp', true);
+                                updateState('showResetPassword', false);
+                            }}
+                        > 회원가입 </button>
+                        <button
+                            onClick={() => {
+                                updateState('resetEmail', '');
+                                // setResetError('');
+                                updateState('showResetPassword', true);
+                                updateState('showSignUp', false);
+                            }}
+                        > 비밀번호 초기화 </button>
+                    </div>
+                </div>
             </div>
             <footer className="Footer">
                 <p>Footer Content</p>

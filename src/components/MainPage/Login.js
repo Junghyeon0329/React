818334acf3d2837f885b5ebae6d04ad67da1e2e4
Junghyeon0329 <sptlsrja@gmail.com>
2025-01-07 21:18:33 +0900

    로그인 회원가입, 비밀번호 찾기 수정

--- src/components/MainPage/Login.js
+++ src/components/MainPage/Login.js
@@ -2,19 +2,23 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import API_URLS from '../../api/apiURLS';
 import { useUser } from '../../contexts/UserContext';
+import AccountModal from './AccountModal';
 
 function Login() {
     const [state, setState] = useState({
         email: '', // 로그인 시 입력한 이메일
         password: '', // 로그인 시 입력한 비밀번호
         error: '', // 로그인 시 발생한 오류 메시지
+        signUpError: '', // 회원가입 오류 메시지
+        ResetError: '', // 비밀번호 리셋 오류 메시지
+        showSignUp: false, // 회원가입 폼의 표시 여부
     });
 
     const updateState = (key, value) => {
         setState((prev) => ({ ...prev, [key]: value }));
     };
 
-    const { loginUser } = useUser();
+    const { user, loginUser } = useUser();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -36,8 +40,46 @@ function Login() {
         } catch (error) {
           updateState('error', '이메일 또는 비밀번호가 잘못되었습니다.');
         }
+    };
+
+    const handleSignUpSubmit = async (e) => {
+        e.preventDefault();
+    
+        const { signUpEmail, signUpPassword } = state;
+    
+        if (signUpEmail === '' || signUpPassword === '') {
+            updateState('signUpError', '필수항목을 모두 입력해주세요.');
+            return;
+        }
+    
+        try {
+            await axios.post(API_URLS.USER, { email: signUpEmail, password: signUpPassword });
+            alert('회원가입 성공!');
+            updateState('showSignUp', false);
+            updateState('signUpError', '');
+        } catch (error) {
+            updateState('signUpError', '사용할 수 없는 이메일입니다.');
+        }
       };
 
+    const handleResetPasswordSubmit = async (e) => {
+        e.preventDefault();
+
+        const { resetEmail } = state;
+
+        if (resetEmail === '') {
+            updateState('ResetError', '이메일을 입력해주세요.');
+            return;
+        }
+
+        try {
+            await axios.put(API_URLS.LOGIN, { email: resetEmail });
+            alert('비밀번호 초기화 링크가 전달 완료되었습니다.');
+            updateState('showResetPassword', false);
+        } catch (error) {
+            alert('비밀번호 초기화 실패');
+        }
+    };
     return (
         <div className="grid-item login">
             <form onSubmit={handleSubmit}>
@@ -88,7 +130,54 @@ function Login() {
                     }}
                 > 비밀번호 초기화 </button>
             </div>
+    
+        {state.showSignUp && (
+            <AccountModal
+            title="회원가입"
+            fields={[
+                {
+                    id: 'signUpEmail',
+                    label: '이메일',
+                    type: 'email',
+                    value: state.signUpEmail,
+                    placeholder: '이메일을 입력하세요',
+                    onChange: (e) => updateState('signUpEmail', e.target.value),
+                },
+                {
+                    id: 'signUpPassword',
+                    label: '비밀번호',
+                    type: 'password',
+                    value: state.signUpPassword,
+                    placeholder: '비밀번호를 입력하세요',
+                    onChange: (e) => updateState('signUpPassword', e.target.value),
+                },
+            ]}
+            onSubmit={handleSignUpSubmit}
+            onClose={() => updateState('showSignUp', false)}
+            error={state.signUpError} // 오류 메시지 전달
+            />
+        )}
+        {state.showResetPassword && (
+            <AccountModal
+            title="비밀번호 초기화"
+            fields={[
+                {
+                id: 'resetEmail',
+                label: '이메일',
+                type: 'email',
+                value: state.resetEmail,
+                placeholder: '이메일을 입력하세요',
+                onChange: (e) => updateState('resetEmail', e.target.value),
+                },
+            ]}
+            onSubmit={handleResetPasswordSubmit}
+            onClose={() => updateState('showResetPassword', false)}
+            error={state.ResetError} // 오류 메시지 전달
+            />
+        )}
+
         </div>
+
     );
 }
 

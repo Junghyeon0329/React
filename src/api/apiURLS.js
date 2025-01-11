f10201c39aeb799e883c0be66a393121c6c20759
Junghyeon0329 <sptlsrja@gmail.com>
2025-01-11 22:21:08 +0900

    파일 다운로드 개발

--- src/api/apiURLS.js
+++ src/api/apiURLS.js
@@ -3,6 +3,7 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;  // 기본 URL
 const API_URLS = {
   LOGIN: `${API_BASE_URL}/login/`,
   USER: `${API_BASE_URL}/user/`,
+  FILE : `${API_BASE_URL}/file/`,
   RESET_PASSWORD: `${API_BASE_URL}/password-reset/`,
   NOTICE : `${API_BASE_URL}/notice/`,
   REFRESH: `${API_BASE_URL}/api/token/refresh/`,
--- src/components/MainPage/DownloadPaper.js
+++ src/components/MainPage/DownloadPaper.js
@@ -1,37 +1,35 @@
 import React from 'react';
 import { useUser } from '../../contexts/UserContext';
-import axios from 'axios';
+import axiosInstance from '../../api/axiosInstance';
+import API_URLS from '../../api/apiURLS';
 import './DownloadPaper.css';
 
 function DownloadPaper() {
-    // useUser 훅을 사용하여 로그인 상태 확인
+    
     const { user } = useUser();
 
-    // 파일 다운로드 처리 함수
     const handleDownload = async (fileType) => {
-        // 로그인이 안되어 있으면 경고창 표시하고 다운로드 중지
+        
         if (!user) {
             alert("로그인 후 사용 가능한 기능입니다.");
             return;
         }
+        try {
+            const response = await axiosInstance.get(API_URLS.FILE, {
+                params: { fileType }
+            });
+            
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement('a');
+            link.href = url;
+            link.setAttribute('download', `${fileType}.csv`); // 파일명 설정
+            document.body.appendChild(link);
+            link.click(); 
+            link.remove();
 
-        // 로그인된 상태에서 파일 다운로드 진행 (API 요청 부분)
-        // try {
-        //     const response = await axios.get(`http://localhost:5000/download/${fileType}`, {
-        //         responseType: 'blob', // 응답을 blob(파일) 형식으로 받음
-        //     });
-
-        //     // 파일을 다운로드할 수 있도록 처리
-        //     const url = window.URL.createObjectURL(new Blob([response.data]));
-        //     const link = document.createElement('a');
-        //     link.href = url;
-        //     link.setAttribute('download', `${fileType}.pdf`); // 파일명 설정
-        //     document.body.appendChild(link);
-        //     link.click(); // 다운로드 실행
-        //     link.remove(); // 링크 제거
-        // } catch (error) {
-        //     console.error("파일 다운로드 중 오류가 발생했습니다.", error);
-        // }
+        } catch (error) {
+            console.error("파일 다운로드 중 오류가 발생했습니다.", error);
+        }
     };
 
     // 파일 항목 목록

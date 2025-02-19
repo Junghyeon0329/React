e57bbd6905e47162b588d8e6b0e6d34b89d8be2a
Junghyeon0329 <sptlsrja@gmail.com>
2025-02-20 01:01:33 +0900

    시계 1차

--- src/components/MainPage/ClockTimer.js
+++ src/components/MainPage/ClockTimer.js
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ClockTimer.css';
 
-function ClockTimer() {
-//   const [date, setDate] = useState(new Date());
+const timezones = {
+  "한국": "Asia/Seoul",
+  "미국": "America/New_York",
+  "대만": "Asia/Taipei",
+};
 
-//   const onChange = (newDate) => {
-//     setDate(newDate);
-//   };
+function ClockTimer() {
+  const [timezone, setTimezone] = useState(timezones["한국"]);
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const localTime = new Intl.DateTimeFormat("en-US", {
+    timeZone: timezone,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
+  }).format(time);
 
   return (
     <div className="grid-item ClockTimer">      
       <div className="calendar-title">
         <h1>현재 시각</h1>
       </div>
-      {/* 
-      <ReactCalendar
-        onChange={onChange}
-        value={date}
-        formatDay={(local, date) => moment(date).format("D")}
-        showNeighboringMonth={false}
-        view="month"
-        locale="ko-KR"
-        tileClassName="react-calendar__tile"
-        calendarType="gregory"
-      /> */}
+      <div className="clock-layout">
+        <div className="timezone-buttons">
+          {Object.keys(timezones).map((country) => (
+            <button key={country} onClick={() => setTimezone(timezones[country])}>
+              {country}
+            </button>
+          ))}
+        </div>
+        <div className="clock-container">
+          <div className="clock">
+            <div
+              className="hand hour-hand"
+              style={{ transform: `rotate(${time.getHours() * 30 + time.getMinutes() * 0.5}deg)` }}
+            />
+            <div
+              className="hand minute-hand"
+              style={{ transform: `rotate(${time.getMinutes() * 6}deg)` }}
+            />
+            <div
+              className="hand second-hand"
+              style={{ transform: `rotate(${time.getSeconds() * 6}deg)` }}
+            />
+          </div>
+          <div className="time-display">{localTime}</div>
+        </div>
+      </div>
     </div>
   );
 }
